d436c9f0613006727d4d02473ad02bfe48dc0df5
BenjaminRasmussen <dumstick228@gmail.com>
2020-07-03 22:43:46 +0200

    gh-pages

---
+++ precache-manifest.e3c62769b966524e8f0a69dfcfe1e9fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff470a36fdfa90f64f69",
+    "url": "/css/app.5c80f38f.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "21d5d23a789209cda724b187e63d46aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e636cd8e2ef39051a91",
+    "url": "/js/about.d6a57a2d.js"
+  },
+  {
+    "revision": "ff470a36fdfa90f64f69",
+    "url": "/js/app.05612d56.js"
+  },
+  {
+    "revision": "1749a7d14aa29e251baa",
+    "url": "/js/chunk-vendors.c97a7ce9.js"
+  },
+  {
+    "revision": "a583df2bc3aec45c9ae7ef6341e36837",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
